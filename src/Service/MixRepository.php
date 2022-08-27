e85ab8cff5b25d1499faf68cd859a088564cfe0d
petrero <petrero@gmail.com>
2022-08-28 00:03:35 +0300

    18.4 Non-Autowireable Services - The Autowire Attribute
      The Autowire Attribute
      - But... let's remove this. Because I want to show you the new way do this... which leverages that same fancy Autowire attribute.
    
      Up here, say #[Autowire()], but instead of just passing a string, say service: 'twig.command.debug'
    
      Using the new Argument
      - I love that! Before we try this, let's actually use the service. Head down to findAll(). Executing a console command manually in your PHP code is totally possible. It's a little weird, but cool! We need to create an $output = new BufferedOutput() object... then we can execute the command by saying $this->twigDebugCommand->run(new ArrayInput()) - this is, sort of, faking the command-line arguments - pass that an empty [] - then . Whatever the command outputs will be set onto that object.
    
      To see if it's working, just dd($output)
    
      Testing time! Refresh... and got it! How fun is that?

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -4,6 +4,8 @@ namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
 use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -15,11 +17,16 @@ class MixRepository {
 		private CacheInterface $cache,
 		#[Autowire('%kernel.debug%')]
 		private bool $isDebug,
+		#[Autowire(service: 'twig.command.debug')]
 		private DebugCommand $twigDebugCommand
 	) {
 	}
 
 	public function findAll():array {
+		$output = new BufferedOutput();
+		$this->twigDebugCommand->run(new ArrayInput([]), $output);
+		dd($output);
+
 		return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
 			$cacheItem->expiresAfter($this->isDebug ? 5 : 60);
 			$response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
