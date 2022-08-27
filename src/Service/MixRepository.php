50b5cf75448cb3735717e65c8860ad90cf1e8bcb
petrero <petrero@gmail.com>
2022-08-27 23:08:35 +0300

    12.6 Dependency Injection
      But we can go further! In PHP 8, there's a new, shorter syntax for creating a property and setting it in the constructor like we're doing. It looks like this. First, I'll move my arguments onto multiple lines... just to keep things organized. Now add the word private in front of each argument. Finish by deleting the properties... as well as the inside of the method.
    
      That might look weird at first, but as soon as you add private, protected, or public in front of a __construct() argument, that creates a property with this name and sets the argument onto that property
    
      So it looks different, but it's the exact same as what we had before.
    
      When we try it... yup! It still works.
    
      Next: I keep saying that the container holds services. That's true! But it also holds one other thing - simple configuration called "parameters".

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -7,12 +7,11 @@ use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MixRepository {
-	private HttpClientInterface $httpClient;
-	private CacheInterface $cache;
 
-	public function __construct(HttpClientInterface $httpClient, CacheInterface $cache) {
-		$this->httpClient = $httpClient;
-		$this->cache = $cache;
+	public function __construct(
+		private HttpClientInterface $httpClient,
+		private CacheInterface $cache
+	) {
 	}
 
 	public function findAll():array {
