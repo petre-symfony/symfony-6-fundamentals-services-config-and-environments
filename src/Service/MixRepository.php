fc2e9be31a45a19e6ec0ba1a197a01aa560447a5
petrero <petrero@gmail.com>
2022-08-27 22:51:12 +0300

    12.1 Dependency Injection
      Our MixRepository service is sort of working. We can autowire it into our controller and the container is instantiating the object and passing it to us. We prove that over here because, when we run the code, it successfully calls the findAll() method.
    
      But.... then it explodes. That's because, inside MixRepository we have two undefined variables. In order for our class to do its job, it needs two services: the $cache service and the $httpClient service.
    
      Autowiring to Methods is a Controller-Only Superpower
      - I keep saying that there are many services floating around inside of Symfony, waiting for us to use them. That's true. But, you can't just grab them out of thin air from anywhere in your code. For example, there's no Cache::get() static method that you can call whenever you want that will return the  service object. Nothing like that exists in Symfony. And that's good! Allowing us to grab objects out of thin air is a recipe for writing bad code.
    
      So how can we get access to these services? Currently, we only know one way: by autowiring them into our controller. But that won't work here. Autowiring services into a method is a superpower that only works for controllers.
    
      Watch: if we added a CacheInterface argument... then went over and refreshed, we'd see:
    
        Too few arguments to function [...]findAll(), 0 passed [...] and exactly 1 expected.
    
      That's because we are calling findAll(). So if findAll() needs an argument, it is our responsibility to pass them: there's no Symfony magic. My point is: autowiring works in controller methods, but don't expect it to work for any other methods.

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,9 +3,10 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class MixRepository {
-	public function findAll():array {
+	public function findAll(CacheInterface $cache):array {
 		return $cache->get('mixes_data', function(CacheItemInterface $cacheItem) use ($httpClient) {
 			$cacheItem->expiresAfter(5);
 			$response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
