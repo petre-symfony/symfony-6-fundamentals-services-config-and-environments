07a0ea511f6b3936b492c7ffbb8188635b8ffe96
petrero <petrero@gmail.com>
2022-08-28 09:57:49 +0300

    22.4. Reading Secrets vs Env Vars The Secrets Vault - php bin/console secrets:list
      The secrets:list Command
      - Ok, remove that dd() and refresh to discover that... locally, everything is broken! Dang! But... of course! It's now using that fake token from the dev vault. It would work ok on production... but how can I fix my local setup so I can keep working?
    
      We could temporarily override the GITHUB_TOKEN secret value in the dev vault by running the secrets:set command. But... that's lame! We would need to be extra careful to not commit the modified, encrypted file.
    
      Before we fix this, I want to show you a really handy command for the vault:
    
        php bin/console secrets:list
      Yup, this shows you all of the secrets in our vault. Pretty cool! And you can even pass --reveal to reveal the value... as long as you have the decrypt key.
    
      You may have noticed that it gives us the value right here... but then says "Local Value" with a blank space. Hmm...
    
      Re-run the command, but this time add --env=prod.
    
        php bin/console secrets:list --reveal --env=prod
      And... same thing! This shows us the real prod value... but there's still this "Local Value" spot with nothing.
    
      This "Local Value" is the key to fixing our broken dev setup: it's a way to override a secret, but only locally on our one machine.
    
      How do you set this local override value? Copy the real GITHUB_TOKEN value, then move over, find .env.local - the same file we've been working in - and say GITHUB_TOKEN= and paste the value we just copied.
    
      Yup! Locally, we're going to take advantage of the fact that environment variables "win" over secrets! Back at your terminal, run
    
        php bin/console secrets:list --reveal
    again. Yes! The official value in the vault is "CHANGEME"... but the local value is our real token which, as we know, will override the secret and be used. If we try the page again... it works!
    
      Okay, team! We're... well... basically done! So as a reward for your hard work on these super important topics, let's celebrate by using Symfony's code generator library: MakerBundle.

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -28,7 +28,6 @@ class MixRepository {
 		$this->twigDebugCommand->run(new ArrayInput([]), $output);
 		dd($output);
 		*/
-		dd($this->githubContentClient);
 		return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
 			$cacheItem->expiresAfter($this->isDebug ? 5 : 60);
 			$response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
