05a1523dd272d829efad75a3c26cfa53bb4c22a7
petrero <petrero@gmail.com>
2022-08-28 19:16:16 +0300

    24.2. Customizing a Command - Filling in execute()
      Filling in execute()
      - When we call our command, very simply, Symfony will call execute()... which is where the fun starts. Inside, we can do whatever we want. It passes us two arguments: $input and $output. If you want to read some input - like the name argument or the yell option, use $input. And if you want to output something, use $output.
    
      But in Symfony, we normally pop these two things into another object called SymfonyStyle. This is helper class makes reading and outputing easier... and fancier.
    
      Ok: let's start by saying $name = $input->getArgument('name'). If we don't have a name, I'll default this to whoever you are. Below, read the option: $shouldYell = $input->getOption('yell')
    
      This is one of the many helper methods on the SymfonyStyle class that help format your output. There's also $io->warning(), $io->note(), and several others.
    
      Let's try it. Spin over and run:
    
        php bin/console app:talk-to-me ryan
      And... oh hello there! If we yell:
    
        php bin/console app:talk-to-me ryan --yell
      THAT WORKS TOO! We can even yell at 'whoever I am':
    
        php bin/console app:talk-to-me --yell
      Awesome! But let's get crazier... by autowiring a service and asking a question interactively on the command line. That's next... and it's the last chapter!

--- src/Command/TalkToMeCommand.php
+++ src/Command/TalkToMeCommand.php
@@ -23,17 +23,16 @@ class TalkToMeCommand extends Command {
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
 		$io = new SymfonyStyle($input, $output);
-		$arg1 = $input->getArgument('arg1');
+		$name = $input->getArgument('name') ?: 'Whoever you are';
+		$shouldYell = $input->getOption('yell');
 
-		if ($arg1) {
-			$io->note(sprintf('You passed an argument: %s', $arg1));
+		$message = sprintf('Hey %s!', $name);
+		if ($shouldYell) {
+			$message = strtoupper($message);
 		}
 
-		if ($input->getOption('option1')) {
-			// ...
-		}
 
-		$io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+		$io->success($message);
 
 		return Command::SUCCESS;
 	}
